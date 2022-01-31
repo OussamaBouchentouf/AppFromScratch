61526d73c5885c6c46e4942442cfc6252cdbc35d
OussamaBouchentouf <oussamabouchentouf6@gmail.com>
2022-01-31 19:17:07 +0100

    Add some feature and fix buggs.

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   Text,
@@ -9,20 +9,36 @@ import {
   ScrollView,
   TextInput,
   Keyboard,
+  Dimensions,
+  Alert,
 } from "react-native";
 import {} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import { commonColors } from "./src/utils/Colors";
 import { Tasks } from "./src/components/tasksContainer";
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 export default function App() {
-  const [task, setTask] = useState(null);
+  const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
+
+  useEffect(() => {
+    loadState();
+  }, []);
+
+  useEffect(() => {
+    saveState();
+  }, [taskItems]);
 
   const handledTask = () => {
     Keyboard.dismiss();
-    setTaskItems([...taskItems, task]);
-    setTask(null);
+    if (task) {
+      setTaskItems([...taskItems, task]);
+      setTask(null);
+    } else createButtonAlert();
   };
 
   const completTask = (index) => {
@@ -31,39 +47,86 @@ export default function App() {
     setTaskItems(itemsCopy);
   };
 
+  const saveState = async () => {
+    try {
+      await AsyncStorage.setItem("tasks", JSON.stringify(taskItems));
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const loadState = async () => {
+    try {
+      const history = await AsyncStorage.getItem("tasks");
+      if (history && JSON.parse(history).length) {
+        setTaskItems(JSON.parse(history));
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const createButtonAlert = () =>
+    Alert.alert("Error", "Please enter a task", [
+      {
+        text: "Cancel",
+        onPress: () => null,
+        style: "cancel",
+      },
+      { text: "OK", onPress: () => null },
+    ]);
+
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.container}
-    >
+    <>
       <View style={styles.titleContainer}>
         <Text style={styles.titleStyle}>Today's tasks</Text>
       </View>
 
-      <View style={styles.bodyContainer}>
-        <ScrollView>
-          {taskItems.map((item, index) => {
-            return (
-               <TouchableOpacity key={index} onPress={() => completTask(index)}>
-                <Tasks text={item} />
-               </TouchableOpacity>
-            );
-          })}
-        </ScrollView>
-      </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        <View style={styles.bodyContainer}>
+          {!taskItems.length ? (
+            <View
+              style={{
+                flex: 1,
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <Text style={styles.simpleText}>Nothing yet :D </Text>
+            </View>
+          ) : (
+            <ScrollView>
+              {taskItems.map((item, index) => {
+                return (
+                  <TouchableOpacity
+                    key={index}
+                    onPress={() => completTask(index)}
+                  >
+                    <Tasks text={item} />
+                  </TouchableOpacity>
+                );
+              })}
+            </ScrollView>
+          )}
+        </View>
 
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.textInput}
-          placeholder="Write a task"
-          keyboardAppearance="dark"
-          onChangeText={(text) => setTask(text)}
-        />
-        <TouchableOpacity style={styles.button} onPress={() => handledTask()}>
-          <Text style={styles.textUnderButton}>+</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Write a task"
+            keyboardAppearance="dark"
+            value={task}
+            onChangeText={(text) => setTask(text)}
+          />
+          <TouchableOpacity style={styles.button} onPress={() => handledTask()}>
+            <Text style={styles.textUnderButton}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </>
   );
 }
 
@@ -74,7 +137,7 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
   },
   titleContainer: {
-    flex: 0.2,
+    height: windowHeight * 0.12,
     alignItems: "center",
     justifyContent: "flex-end",
     backgroundColor: "#bbeefa",
@@ -85,9 +148,9 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   simpleText: {
-    fontSize: 20,
-    color: commonColors.Black,
-    fontWeight: "800",
+    fontSize: 15,
+    fontStyle: "italic",
+    fontWeight: "600",
   },
   bodyContainer: {
     flex: 1,
